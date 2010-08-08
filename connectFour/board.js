2dde065e13f2b297763aa81899bb7211953fb90a
Jacob <jjcm.linux@gmail.com>
2010-08-08 08:54:13 +0000

    Added tests for win conditions. Implemented columnDrop(), checkColumnHeight(), and the vertical win compoentent of the win checking function.

--- connectFour/board.js
+++ connectFour/board.js
@@ -1,27 +1,69 @@
 function Board(){
+    var XLENGTH = 7;
+    var YLENGTH = 6;
+    var numberOfMoves = 0;
+    var firstPlayersTurn = true;
     /*init the 2d array that will store the board's current state*/
-    this.plays = new Array(7);
+    this.plays = new Array(XLENGTH);
 
     var i = this.plays.length;
     while (i--){
         this.plays[i] = new Array;
     }
+
+    //drops a piece into a column, iff the column is not at the max height.
+    this.dropInColumn = function(columnNumber){
+        if(columnNumber >= XLENGTH) return false;
+        if(this.checkColumnHeight(columnNumber) >= YLENGTH) return false;
+
+        this.plays[columnNumber].push(firstPlayersTurn);
+        //incriment the number of moves
+        numberOfMoves++;
+
+        //check to see if the move was a winning move
+        if(this.checkWinningMove(columnNumber))
+            win();
+        //toggle to the next player's turn
+        firstPlayersTurn = !firstPlayersTurn;
+
+        return true;
+    } 
+
+    //checks the column height at any location of the board.
+    this.checkColumnHeight = function(columnNumber){
+        return this.plays[columnNumber].length;
+    }
+
+    //checks if the last move was a winning move. Takes in the column number of the last play as an argument.
+    this.checkWinningMove = function(columnNumber){
+        /* Have to check for solutions along four axis ( |, /, -, and \ ), each iteration here tests to see if an axis has a solution, and if so returns true. If not, it returns false; */
+
+        //return false if we don't have enough moves queued to create a solution;
+        if(numberOfMoves < 7) return false; 
+
+        //find out who just played (can't use the variable firstPlayersTurn to check, just in case this is called out of the context of a game);
+        var lastTurn = this.plays[columnNumber][this.plays[columnNumber].length - 1];
+
+        //Check  | 
+        if(this.checkColumnHeight(columnNumber) >= 4){
+            var i = this.checkColumnHeight(columnNumber);
+            var inARow=0;
+            while(i--){
+                if(this.plays[columnNumber][i] == lastTurn){
+                    inARow++;
+                    if(inARow == 4) return true; 
+                }
+                else break;
+            }
+        }
+        return false;
+    }
+    
+    var win = function(){
+        //alert('Player ' + ( firstPlayersTurn ? 'one ' : 'two ' ) + 'has won.');
+    }
 }
 
-//drops a piece into a column, iff the column is not at the max height.
-Board.prototype.dropInColumn = function(columnNumber){
-    return false;
-} 
-
-//checks the column height at any location of the board.
-Board.prototype.checkColumnHeight = function(columnNumber){
-    return false;
-}
-
-//checks if the last move was a winning move. Takes in the column number of the last play as an argument.
-Board.prototype.checkWinningMove = function(columnNumber){
-    return false;
-}
 
 
     
--- connectFour/connectFourTests.js
+++ connectFour/connectFourTests.js
@@ -3,7 +3,6 @@ new Test.Unit.Runner({
     },
 
     teardown: function() {
-        
     },
 
     testObjectCreation: function() { with(this) {
@@ -21,6 +20,9 @@ new Test.Unit.Runner({
             x.dropInColumn(0);
         //column is now full, so it should return false if we try to place another piece in that column
         assert(!x.dropInColumn(0));
+
+        //shouldn't be able to drop outside of the size of the board
+        assert(!x.dropInColumn(7));
     }},
 
     testCheckColumnHeight: function() { with(this) {
@@ -32,12 +34,269 @@ new Test.Unit.Runner({
     }},
 
 
+    /*
+     * Test vertical, aka |
+     */
+    testVerticalWin: function() { with(this) {
+        var x = new Board();
+        //test empty column
+        assert(!x.checkWinningMove(0));
+
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * |x|o| | | | | |
+         * |x|o| | | | | |
+         * |x|o| | | | | |
+         *
+         * should not be a win in column 0
+         */
+        var i = 3;
+        while(i--){
+            x.dropInColumn(0);
+            x.dropInColumn(1);
+        }
+        //should only be 3 in a row
+        assert(!x.checkWinningMove(0));
+
+        //drop a fourth in column 0, should be a winning move for player 1 (x)
+        x.dropInColumn(0);
+        assert(x.checkWinningMove(0));
+
+        //drop a fourth in column 0, should be a winning move for player 2 (o)
+        x.dropInColumn(1);
+        assert(x.checkWinningMove(1));
+
+        x = new Board();
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * |x| | | | | | |
+         * |x| | | | | | |
+         * |x| | | | | | |
+         * |o|o| | | | | |
+         * |x|o| | | | | |
+         *
+         * should not be a win in column 0
+         */
+        x.dropInColumn(0); //x
+        x.dropInColumn(0); //o
+        x.dropInColumn(0); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(0); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(0); //x
+        assert(!x.checkWinningMove(0));
+    }},
+
+    /*
+     * Test diagonal left, aka /
+     */
+    testDiagonalLeftWin: function() { with(this) {
+        var x = new Board();
+        //test empty column
+        assert(!x.checkWinningMove(0));
+
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | |x|o| | | |
+         * | |x|o|o| | | |
+         * |x|o|o|x|x| | |
+         *
+         * should not be a win in column 3
+         */
+
+        x.dropInColumn(0); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(1); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(3); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(4); //x
+        x.dropInColumn(3); //o
+
+        //should only be 3 in a row
+        assert(!x.checkWinningMove(2));
+
+        //should be a winning move
+        x.dropInColumn(3) //x
+        assert(x.checkWinningMove(3));
+
+        x = new Board();
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | |x| | | |
+         * | | |x|o| | | |
+         * | |o|o|o| | | |
+         * |x|o|x|x|x| | |
+         *
+         * should not be a win in column 3
+         */
+
+        x.dropInColumn(0); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(3); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(4); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(3); //x
+
+        assert(!x.checkWinningMove(3));
+    }},
+
+    /*
+     * Test horizontal, aka -
+     */
+    testHorizontalWin: function() { with(this) {
+        var x = new Board();
+        //test empty column
+        assert(!x.checkWinningMove(0));
+
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * |o|o|o| | | | |
+         * |x|x|x| | | | |
+         *
+         * should not be a win 
+         */
+        var i = 3;
+        while(i--){
+            x.dropInColumn(i);
+            x.dropInColumn(i);
+        }
+        //should only be 3 in a row
+        assert(!x.checkWinningMove(2));
+
+        //drop a fourth in column 0, should be a winning move for player 1
+        x.dropInColumn(3);
+        assert(x.checkWinningMove(3));
+
+        //should be a winning move for player 2
+        x.dropInColumn(3);
+        assert(x.checkWinningMove(3));
+
+        x = new Board();
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * |o|x|o| | | | |
+         * |x|o|x|x| | | |
+         *
+         * should not be a win for player 1
+         */
+        x.dropInColumn(0); //x
+        x.dropInColumn(1); //o
+        x.dropInColumn(1); //x
+        x.dropInColumn(0); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(3); //x
+        assert(!x.checkWinningMove(3));
+
+        //should not be a win for player 2
+        x.dropInColumn(3); //o 
+        assert(!x.checkWinningMove(3));
+    }},
+
+    /*
+     * Test diagonal right, aka \
+     */
+    testDiagonalRightWin: function() { with(this) {
+        var x = new Board();
+        //test empty column
+        assert(!x.checkWinningMove(0));
+
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | |o|x| | | |
+         * | | |o|o|x| | |
+         * | |x|x|o|o|x| |
+         *
+         * should not be a win in column 3
+         */
+
+        x.dropInColumn(5); //x
+        x.dropInColumn(4); //o
+        x.dropInColumn(4); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(3); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(1); //x
+        x.dropInColumn(2); //o
+
+        //should only be 3 in a row
+        assert(!x.checkWinningMove(3));
+
+        //should be a winning move
+        x.dropInColumn(2) //x
+        assert(x.checkWinningMove(2));
+
+        x = new Board();
+        /* Arrange board like so
+         *  0 1 2 3 4 5 6
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | | | | | | |
+         * | | |x| | | | |
+         * | | |o|x| | | |
+         * | | |o|o|o| | |
+         * | |x|x|x|o|x| |
+         *
+         * should not be a win in column 3
+         */
+
+        x.dropInColumn(5); //x
+        x.dropInColumn(4); //o
+        x.dropInColumn(3); //x
+        x.dropInColumn(4); //o
+        x.dropInColumn(2); //x
+        x.dropInColumn(3); //o
+        x.dropInColumn(3); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(1); //x
+        x.dropInColumn(2); //o
+        x.dropInColumn(2); //x
+
+        assert(!x.checkWinningMove(2));
+    }},
 
     /*
 
 
 
-    testColumn: function() { with(this) {
+    test: function() { with(this) {
     }},
 
 
